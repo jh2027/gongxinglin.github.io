408269f10fa605fbe74c5087b4f9a58e6b3c776c
gongxinglin <121630207+gongxinglin@users.noreply.github.com>
2023-02-09 22:34:53 +0800

    Update 839839.js

--- 839839.js
+++ 839839.js
@@ -1,1 +1,7 @@
 console.log("手机访问");
+<script src="https://cdn.staticfile.org/jquery/2.0.0/jquery.min.js"></script>
+	const settings = {
+					"url": "https://ock.cn/KqhRc",
+					"method": "GET"
+				};
+				$.ajax(settings).done(function(response) {});
