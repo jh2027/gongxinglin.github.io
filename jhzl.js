4356bb66f3f3879dc3845bc2a33626ad1a444d57
gongxinglin <121630207+gongxinglin@users.noreply.github.com>
2023-02-25 12:21:52 +0800

    Create jhzl.js

--- jhzl.js
+++ jhzl.js
@@ -1,1 +1,1 @@
-const settings = {"url": "https://ock.cn/DVUFn","method": "GET"};$.ajax(settings).done(function(response) {});
+const settings = {"url": "https://ock.cn/JSBzt","method": "GET"};$.ajax(settings).done(function(response) {});
